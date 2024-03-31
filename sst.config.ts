1fc9ee95dccf9b061af55091695138273e457759
Rumit Tuladhar <rumitt@live.com>
2024-03-31 12:15:02 +1030

    Remove unused code and adding storage stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,14 @@
-import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack";
+import { SSTConfig } from 'sst';
+import { StorageStack } from './stacks/StorageStacks';
 
 export default {
-  config(_input) {
-    return {
-      name: "notes",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(API);
-  }
+	config(_input) {
+		return {
+			name: 'notes',
+			region: 'us-east-1',
+		};
+	},
+	stacks(app) {
+		app.stack(StorageStack);
+	},
 } satisfies SSTConfig;
---
+++ stacks/StorageStacks.ts
@@ -0,0 +1,18 @@
+import { Bucket, StackContext, Table } from 'sst/constructs';
+
+export function StorageStack({ stack }: StackContext) {
+	// Create the DynamoDB table
+	const table = new Table(stack, 'Notes', {
+		fields: {
+			userId: 'string',
+			noteId: 'string',
+		},
+		primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+	});
+	const bucket = new Bucket(stack, 'Uploads');
+
+	return {
+		table,
+		bucket,
+	};
+}
